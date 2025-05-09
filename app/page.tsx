94635b79e084c3d518a406ef2227922e70263349
LeonM95 <leomasis95@gmail.com>
2025-05-08 21:18:37 -0600

    Added here section, added carosel with animation, install shadcn compenents, add styyles to app.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
   // fetch products from stripe
@@ -10,20 +10,47 @@ export default async function Home() {
     expand: ["data.default_price"],
     limit: 5,
   });
-
+  console.log(products.data);
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to my Ecommerce</h2>
-            <p>Discover the latest producs at the best prices.</p>
+      {/* section for image hero */}
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to my Ecommerce
+            </h2>
+            <p className="text-neutral-600">
+              Discover the latest producs at the best prices.
+            </p>
             {/* this button comes from shadcn*/}
-            <Button asChild variant="default">
-              <Link href="/products">Browse all products</Link>
+            <Button
+              asChild
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse all products
+              </Link>
             </Button>
           </div>
+
+          <Image
+            alt="Hero Image"
+            src={products.data[1].images[0]}
+            className="rounded"
+            width={450}
+            height={450}
+          />
         </div>
+      </section>
+      {/* section for carrusel */}
+      <section className="py-8">
+        {/* caroulse component */}
+        <Carousel products={products.data} />
       </section>
     </div>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //add stripe domain to allow to serve images
+  images: {
+    domains: ["files.stripe.com"],
+  },
 };
 
 export default nextConfig;
